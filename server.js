ffaae4012014cd79b660a1223f05e15e62d7027b
Jayavel <sathya.jayavel@accenture.com>
2023-06-01 19:50:46 +0530

    post req update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/users', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  console.log(req.body.data);
+  console.log(JSON.stringify(req.body.data));
   res.send("Completed").status(200);
 })
 
